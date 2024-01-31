7b4372d5e78d69fcd70e0f1f42c9c7626df36a5a
DawidRaczek <dawid.raczek6666@gmail.com>
2024-02-01 00:56:49 +0100

    added arrow

--- src/components/fronttitle.jsx
+++ src/components/fronttitle.jsx
@@ -11,7 +11,7 @@ const Wrapper = styled.div`
 
 const Texttitle = styled.h1`
     color: white;
-    text-align; center;
+    text-align: center;
     margin-bottom: 10px;
     font-size: 55px;
     letter-spacing: 0;
@@ -27,6 +27,49 @@ const Undertitle = styled.p`
     font-family: Relative;
 `
 
+const Arrowwrapper = styled.div`
+    position: absolute;
+    bottom: 30px;
+    right: 0;
+    width: 44px;
+    height: 80px;
+    left: 50%;
+    transform: translateX(-50%) scaleX(.5) scaleY(.5);
+    cursor: pointer;
+    z-index: 999;
+`
+const Arrow = styled.div`
+    height: 80px;
+    width: 1px;
+    text-align: center;
+    background-color: #aaa;
+    position: absolute;
+    transform-origin: 50% 0;
+    margin-left: 22px;
+    top: 0;
+`
+
+const Leftarrow = styled.div`
+    position: absolute;
+    bottom: 0;
+    width: 1px;
+    height: 16px;
+    transform-origin: 50% 100%;
+    background-color: #aaa;
+    transform: rotate(45deg);
+    margin-left: 22px;
+`
+const Rightarrow = styled.div`
+    position: absolute;
+    bottom: 0;
+    width: 1px;
+    height: 16px;
+    transform-origin: 50% 100%;
+    background-color: #aaa;
+    transform: rotate(-45deg);
+    margin-left: 22px;
+`
+
 const fronttitle = () => {
 
 return (
@@ -39,6 +82,11 @@ return (
                 Frontend Developer
             </Undertitle>
         </Wrapper>
+        <Arrowwrapper>
+            <Rightarrow></Rightarrow>
+            <Arrow></Arrow>
+            <Leftarrow></Leftarrow>
+        </Arrowwrapper>
     </>
 )
 }
